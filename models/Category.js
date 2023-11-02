4a1ff2b70fcbee21b1d0e9e355bd5924a9a101ac
Janet Webster <twixmixy@gmail.com>
2023-11-01 23:22:54 -0400

    properties and parameters for the category model

--- models/Category.js
+++ models/Category.js
@@ -7,9 +7,17 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
-    // what is a category?
-    // id, category name
-    // reqs? type?
+    // properties and parameters for the category model
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
