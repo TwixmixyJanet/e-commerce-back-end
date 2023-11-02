a27a7df6a325212b411b638ed84a415facd4f416
Janet Webster <twixmixy@gmail.com>
2023-11-02 18:18:22 -0400

    Tag update route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -56,8 +56,33 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const updateTag = await Tag.update({
+      tag_name: req.body.tagName
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    });
+
+    const tagID = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product }]
+    });
+
+    if (!tagID) {
+      res.status(200).json({ message: 'Tag not found' });
+      return;
+    };
+
+    res.status(200).json(updateTag);
+    console.log('Tag is updated!');
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
