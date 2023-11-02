1173a0dbb13186778e355dc60bcf553c7c36a0df
Janet Webster <twixmixy@gmail.com>
2023-11-01 23:17:51 -0400

    Creating all the relationships between the models

--- models/index.js
+++ models/index.js
@@ -4,15 +4,31 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+// Creating all of the relationships between the models.
+
 // Products belongsTo Category
-// look up syntax of comments
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
 
 // Products belongToMany Tags (through ProductTag)
 // many to many relationship
+// https://sequelize.org/docs/v6/advanced-association-concepts/eager-loading/ - research for "through" to connect the tables many to many relationship
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+});
 
 module.exports = {
   Product,
