d473987dc8d19af60c53a4814c1ab3722edd75d7
Janet Webster <twixmixy@gmail.com>
2023-11-01 22:29:59 -0400

    new category route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -44,17 +44,29 @@ router.get('/:id', async (req, res) => {
   };
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
   // get the body and get the contents and insert into sequelize
   // category(DOT)create 
   // whatever we created we can return it back off the res.json
+  try {
+    const newCategory = await Category.create({
+      category_name: req.body.categoryName
+    });
+
+    res.status(200).json(newCategory);
+    console.log('Added new category!');
+
+  } catch (err) {
+    res.status(500).json(err);
+  };
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
   // taking in an id as a parameter and also receiving a req.body
   // sequelize update
+
 });
 
 router.delete('/:id', (req, res) => {
@@ -62,6 +74,7 @@ router.delete('/:id', (req, res) => {
   // DESTROY
   // destroying based off the req.params.id
   // res.json to let the server know it's gone
+  
 });
 
 module.exports = router;
