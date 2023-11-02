93024c410704c49aaf9b3d6fe4c376f0ed415369
Janet Webster <twixmixy@gmail.com>
2023-11-01 22:46:25 -0400

    DESTROY category route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -81,7 +81,7 @@ router.put('/:id', async (req, res) => {
     });
 
     if (!categoriesID) {
-      res.status(200).json({ message: 'No categories found'});
+      res.status(200).json({ message: 'No categories found' });
       return;
     };
 
@@ -92,12 +92,28 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
   // DESTROY
   // destroying based off the req.params.id
   // res.json to let the server know it's gone
+  try {
+    const destroyCategory = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
 
+    if (!destroyCategory) {
+      res.status(200).json({ message: 'No categories found' });
+      return;
+    };
+
+    res.status(200).json(destroyCategory);
+    console.log('This category has been destroyed!')
+  } catch {
+    res.status(500).json(err);
+  };
 });
 
 module.exports = router;
