95b5623d58f235baaa432371e3340ab0e2ee828f
Janet Webster <twixmixy@gmail.com>
2023-11-02 18:22:10 -0400

    delete destroy product route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -120,22 +120,22 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
   try {
-    const destroyCategory = await Category.destroy({
+    const destroyProduct = await Product.destroy({
       where: {
         id: req.params.id
       }
     });
 
-    if (!destroyCategory) {
-      res.status(200).json({ message: 'No categories found' });
+    if (!destroyProduct) {
+      res.status(200).json({ message: 'Could not find that product' });
       return;
     };
 
-    res.status(200).json(destroyCategory);
-    console.log('This category has been destroyed!')
+    res.status(200).json(destroyProduct);
+    console.log('This product has been destroyed!')
   } catch {
     res.status(500).json(err);
   };
