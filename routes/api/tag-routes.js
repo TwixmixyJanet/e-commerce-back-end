008775ce6238b81be7d2c7787fc90cefad63937d
Janet Webster <twixmixy@gmail.com>
2023-11-02 20:20:40 -0400

    Still testing all PUT requests

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -44,9 +44,7 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   // create a new tag
   try {
-    const newTag = await Tag.create({
-      tag_name: req.body.tagName
-    });
+    const newTag = await Tag.create(req.body);
 
     res.status(200).json(newTag);
     console.log('New tag created!');
